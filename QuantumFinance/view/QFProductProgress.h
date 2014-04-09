adb4569edd21db499c5e42c698cd98cde13b94dd
gongtao <gongtao@jike.com>
2014-04-09 13:38:06 +0800

    添加文件

---
+++ QuantumFinance/view/QFProductProgress.h
@@ -0,0 +1,13 @@
+//
+//  QFProductProgress.h
+//  QuantumFinance
+//
+//  Created by 龚涛 on 4/9/14.
+//  Copyright (c) 2014 龚涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QFProductProgress : UIView
+
+@end
---
+++ QuantumFinance/view/QFProductProgress.m
@@ -0,0 +1,31 @@
+//
+//  QFProductProgress.m
+//  QuantumFinance
+//
+//  Created by 龚涛 on 4/9/14.
+//  Copyright (c) 2014 龚涛. All rights reserved.
+//
+
+#import "QFProductProgress.h"
+
+@implementation QFProductProgress
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
