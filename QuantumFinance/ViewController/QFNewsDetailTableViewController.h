c5fe79d58d523c6df68e700eafdf76bae7c9b8c2
zhumanyu <259571082@qq.com>
2014-04-10 15:32:52 +0800

    add file

---
+++ QuantumFinance/ViewController/QFNewsDetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QFNewsDetailTableViewController.h
+//  QuantumFinance
+//
+//  Created by 曼瑜 朱 on 14-4-10.
+//  Copyright (c) 2014年 龚涛. All rights reserved.
+//
+
+#import "GTTableViewController.h"
+
+@interface QFNewsDetailTableViewController : GTTableViewController
+
+@end
---
+++ QuantumFinance/ViewController/QFNewsDetailTableViewController.m
@@ -0,0 +1,38 @@
+//
+//  QFNewsDetailTableViewController.m
+//  QuantumFinance
+//
+//  Created by 曼瑜 朱 on 14-4-10.
+//  Copyright (c) 2014年 龚涛. All rights reserved.
+//
+
+#import "QFNewsDetailTableViewController.h"
+
+@interface QFNewsDetailTableViewController ()
+
+@end
+
+@implementation QFNewsDetailTableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
