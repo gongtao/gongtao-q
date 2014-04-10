2de3118c191608b0c6fae4da68ed0935a5a2bffe
zhumanyu <259571082@qq.com>
2014-04-11 00:30:42 +0800

    add file

---
+++ QuantumFinance/view/TableCell/QFCommentCell.h
@@ -0,0 +1,13 @@
+//
+//  QFCommentCell.h
+//  QuantumFinance
+//
+//  Created by 曼瑜 朱 on 14-4-11.
+//  Copyright (c) 2014年 龚涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QFCommentCell : UITableViewCell
+
+@end
---
+++ QuantumFinance/view/TableCell/QFCommentCell.m
@@ -0,0 +1,29 @@
+//
+//  QFCommentCell.m
+//  QuantumFinance
+//
+//  Created by 曼瑜 朱 on 14-4-11.
+//  Copyright (c) 2014年 龚涛. All rights reserved.
+//
+
+#import "QFCommentCell.h"
+
+@implementation QFCommentCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
