351263f3500603297705b198dc86f2a10ba96c02
zhumanyu <259571082@qq.com>
2014-04-14 19:04:47 +0800

    add file

---
+++ QuantumFinance/ViewController/QFCollectionTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QFCollectionTableViewController.h
+//  QuantumFinance
+//
+//  Created by 曼瑜 朱 on 14-4-14.
+//  Copyright (c) 2014年 龚涛. All rights reserved.
+//
+
+#import "GTTableViewController.h"
+
+@interface QFCollectionTableViewController : GTTableViewController
+
+@end
---
+++ QuantumFinance/ViewController/QFCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  QFCollectionViewController.h
+//  QuantumFinance
+//
+//  Created by 曼瑜 朱 on 14-4-14.
+//  Copyright (c) 2014年 龚涛. All rights reserved.
+//
+
+#import "QFSubBaseViewController.h"
+
+@interface QFCollectionViewController : QFSubBaseViewController
+
+@end
