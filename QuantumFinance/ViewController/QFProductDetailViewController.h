77734d0d58e4ab9bea0219d00a522eb5e2bb7a01
gongtao <gongtao@jike.com>
2014-04-15 11:07:42 +0800

    添加产品详情界面

---
+++ QuantumFinance/ViewController/QFProductDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  QFProductDetailViewController.h
+//  QuantumFinance
+//
+//  Created by 龚涛 on 4/15/14.
+//  Copyright (c) 2014 龚涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QFProductDetailViewController : UIViewController
+
+@end
---
+++ QuantumFinance/ViewController/QFProductDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  QFProductDetailViewController.m
+//  QuantumFinance
+//
+//  Created by 龚涛 on 4/15/14.
+//  Copyright (c) 2014 龚涛. All rights reserved.
+//
+
+#import "QFProductDetailViewController.h"
+
+@interface QFProductDetailViewController ()
+
+@end
+
+@implementation QFProductDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
